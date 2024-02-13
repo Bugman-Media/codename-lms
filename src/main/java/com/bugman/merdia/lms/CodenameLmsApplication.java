159949cedc73329ef9d52e4c4c21062b4190d20a
Bagniz <salimkhengui@gmail.com>
2024-02-13 23:58:08 +0100

    Initializing spring boot project

---
+++ src/main/java/com/bugman/merdia/lms/CodenameLmsApplication.java
@@ -0,0 +1,13 @@
+package com.bugman.merdia.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodenameLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodenameLmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bugman/merdia/lms/CodenameLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugman.merdia.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodenameLmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
